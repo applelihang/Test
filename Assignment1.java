561f46eeed2687f6b9e3a00d37f631cf25537f7a
apple_lihang <apple_lihang@hotmail.com>
2015-06-05 15:54:24 -0400

    test

---
+++ Assignment1.java
@@ -0,0 +1,11 @@
+package assignmentMay28;
+/*
+ * Print all ASCII codes from 0 to 255
+ */
+public class Assignment1 {
+	public static void main(String[] args){
+		for(int i=0;i<=255;i++){
+			System.out.println("ASSCII code of "+i+" is : "+(char)i);
+		}
+	}
+}

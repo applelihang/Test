e44905bdd3e614663f08870d64c2291f1af41584
applelihang <apple_lihang@hotmail.com>
2015-06-05 15:56:04 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -6,6 +6,7 @@ public class Assignment1 {
 	public static void main(String[] args){
 		for(int i=0;i<=255;i++){
 			System.out.println("ASSCII code of "+i+" is : "+(char)i);
+			//System.
 		}
 	}
 }
